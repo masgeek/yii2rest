fc33169990f34dd82c375669c165279fafc2e32e
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-27 14:48:01 +0300

    Test

--- client/app.js
+++ client/app.js
@@ -1,11 +1,13 @@
 /**
  * Created by barsa on 3/27/2017.
  */
+'use strict'
 
 angular.module('demo', [])
     .controller('Hello', function($scope, $http) {
-        $http.get('http://rest-service.guides.spring.io/greeting').
+        //$http.get('http://rest-service.guides.spring.io/greeting').
+        $http.get('http://localhost/yii2rest/api/v1/products').
         then(function(response) {
-            $scope.greeting = response.data;
+            $scope.products = response.data;
         });
     });
