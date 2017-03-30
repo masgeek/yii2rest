1e3ed917dee655c780792a06d36a25cb19bb479a
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-30 15:14:24 +0300

    Updates

--- client/js/app.js
+++ client/js/app.js
@@ -6,31 +6,35 @@
 var app = angular.module('app', [
     'ngResource', //allows us to use $resource to call rest urls
     'ngRoute',      //$routeProvider
-    'mgcrea.ngStrap'//bs-navbar, data-match-route directives
+    'mgcrea.ngStrap',//bs-navbar, data-match-route directives
+    'ui.router',
+    'app.services',
+    'app.controllers',
 ]);
 
 
-app.factory('Product', function ($resource) {
-    return $resource('../api/v1/products'); // Note the full endpoint address
+app.config(function ($stateProvider) {
+    $stateProvider.state('products', { // state for showing all movies
+        url: '/products',
+        templateUrl: 'partials/products.html',
+        controller: 'ProductListController'
+    }).state('viewProduct', { //state for showing single movie
+        url: '/product/:id/view',
+        templateUrl: 'partials/product-view.html',
+        controller: 'ProductViewController'
+    }).state('newProduct', { //state for adding a new movie
+        url: '/products/new',
+        templateUrl: 'partials/product-add.html',
+        controller: 'MovieCreateController'
+    }).state('editProduct', { //state for updating a movie
+        url: '/product/:id/edit',
+        templateUrl: 'partials/product-edit.html',
+        controller: 'ProductEditController'
+    });
+}).run(function ($state) {
+    $state.go('products'); //make a transition to products state when app starts
 });
 
-/*
- app.controller('Product', function ($scope, $http) {
- //$http.get('http://rest-service.guides.spring.io/greeting').
- $http.get('../api/v1/products').then(function (response) {
- $scope.products = response.data;
- });
- });*/
 
-/*
-app.controller('oneProduct', function ($scope, Product) {
-    var entry = Product.get({ id: $scope.id }, function() {
-        console.log(entry);
-    });
-});*/
 
-app.controller('allProducts', function ($scope, Product) {
-    var entries = Product.query(function () {//get all products
-        $scope.products = entries;
-    });
-});
+
--- client/js/services.js
+++ client/js/services.js
@@ -2,10 +2,16 @@
  * Created by barsa on 3/30/2017.
  */
 
-angular.module('app.services', []).factory('Movie', function($resource) {
+angular.module('app.services', []).factory('Product', function($resource) {
     return $resource('../api/v1/products/:id', { id: '@id' }, {
         update: {
             method: 'PUT'
         }
     });
 });
+
+angular.module('app.services').service('popupService', ['$window', function ($window) {
+    this.showPopup = function (message) {
+        return $window.confirm(message); //Ask the users if they really want to delete
+    }
+}]);
