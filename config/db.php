f15da3cdd2ad45b6a5d531d5b997042e15731c95
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-31 13:16:52 +0300

    models removal migration files removal

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tsobu',
+    'dsn' => 'mysql:host=localhost;dbname=crossover',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
